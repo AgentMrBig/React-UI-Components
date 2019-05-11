4ebeae5089642756b169b81b41190c87c3e18ef0
Eric Hendrix <agentmrbig@gmail.com>
2019-05-11 00:01:19 -0400

    finally getting my styling base down, gotta go to bed, work tomorrow

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,13 +4,17 @@ import './components/ButtonComponents/Button.css'
 
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import NumberButton from './components/ButtonComponents/NumberButton'
+import ActionButton from './components/ButtonComponents/ActionButton'
 
 const App = () => {
   return (
     <div className='calcContainer'>
       <CalculatorDisplay />
       <div className='buttons'>
-        <NumberButton buttonStyle={'clearButton'} text='CLEAR' />
+        <div className='firstButtonRow'>
+          <NumberButton buttonStyle={'clearButton'} text='CLEAR' />
+          <NumberButton buttonStyle={'actionButton'} text='&divide;' />
+        </div>
       </div>
     </div>
   )
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from 'react'
+import './Button.css'
+
+const ActionButton = props => {
+  //   return <div className='numButton' />
+
+  return (
+    <div className={props.buttonStyle || 'actionButton'}>{props.text || 0}</div>
+  )
+}
+
+export default ActionButton
