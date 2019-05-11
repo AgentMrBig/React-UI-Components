4c72205bef04e24a398c2d0c6823695e8f80a724
Eric Hendrix <agentmrbig@gmail.com>
2019-05-10 21:55:21 -0400

    Initial commit on project II, setting up base components, testing

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from 'react'
+import './Button.css'
+
+const NumberButton = props => {
+  //   return <div className='numButton' />
+
+  return (
+    <div className={props.buttonStyle || 'numButton'}>{props.text || 0}</div>
+  )
+}
+
+export default NumberButton
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Display.css';
+import React from 'react'
+import './Display.css'
+
+const CalculatorDisplay = () => {
+  return <div className='calcDisplay' />
+}
+
+export default CalculatorDisplay
