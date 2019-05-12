d7772879cc5a948fbfc063688df03489e87642be
Eric Hendrix <agentmrbig@gmail.com>
2019-05-12 01:14:32 -0400

    mvp complete

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -13,7 +13,29 @@ const App = () => {
       <div className='buttons'>
         <div className='firstButtonRow'>
           <NumberButton buttonStyle={'clearButton'} text='CLEAR' />
-          <NumberButton buttonStyle={'actionButton'} text='&divide;' />
+          <ActionButton buttonStyle={'actionButton'} text='&divide;' />
+        </div>
+        <div className='firstButtonRow'>
+          <NumberButton buttonStyle={'numButton'} text='7' />
+          <NumberButton buttonStyle={'numButton'} text='8' />
+          <NumberButton buttonStyle={'numButton'} text='9' />
+          <ActionButton buttonStyle={'actionButton'} text='	&times;' />
+        </div>
+        <div className='firstButtonRow'>
+          <NumberButton buttonStyle={'numButton'} text='4' />
+          <NumberButton buttonStyle={'numButton'} text='5' />
+          <NumberButton buttonStyle={'numButton'} text='6' />
+          <ActionButton buttonStyle={'actionButton'} text='&minus;' />
+        </div>
+        <div className='firstButtonRow'>
+          <NumberButton buttonStyle={'numButton'} text='4' />
+          <NumberButton buttonStyle={'numButton'} text='5' />
+          <NumberButton buttonStyle={'numButton'} text='6' />
+          <ActionButton buttonStyle={'actionButton'} text='&#43;' />
+        </div>
+        <div className='firstButtonRow'>
+          <NumberButton buttonStyle={'clearButton'} text='0' />
+          <ActionButton buttonStyle={'actionButton'} text='&#61;' />
         </div>
       </div>
     </div>
